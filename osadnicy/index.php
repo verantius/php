5867f33751a9c29eff16459994e564f8fe93d74a
Adam <adam.midtt@gmail.com>
2021-08-30 21:02:47 +0200

    index

---
+++ osadnicy/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Osadnicy</title>
+</head>
+<body>
+    
+<?php
+echo "<h1>Osadnicy Online</h1>";
+?>
+Osadnicy
+    <form action="zaloguj.php" method="post">
+        Login: <br/> <input type="text" name="login"/> <br/>
+        Hasło: <br/> <input type="password" name="haslo"/> <br/><br/>
+        <input type="submit" value="zaloguj się lub nie"/>
+
+
+
+
+    </form>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
