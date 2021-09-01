98f8f11d7a1b9560a2641eae70729c2ab79b636c
Adam <adam.midtt@gmail.com>
2021-09-01 19:58:05 +0200

    isset

--- osadnicy/zaloguj.php
+++ osadnicy/zaloguj.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if ((!isset($_POST['login'])) || (!isset($_POST['haslo'])))
+{
+    header('Location: index.php');
+    exit();
+}
 require_once "connect.php";
 
 $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
