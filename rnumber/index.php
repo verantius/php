41be53b183b321c576ef540b83dd3cc45d058abc
Adam <adam.midtt@gmail.com>
2021-09-06 20:51:34 +0200

    rnd

--- rnumber/index.php
+++ rnumber/index.php
@@ -7,31 +7,67 @@
         <title>Oblicz math</title>
     </head>
     <body>
+        <?php
+        $rand1 = (rand(10,100));
+        $rand2 = (rand(2,10));
+        $rand3 = (rand(3,8));
+        $rand4 = (rand(2,7));
+        $rand5 = 3;
+        echo "<br>";
+        echo "wylosowana liczba: ".$rand1." ".$rand2." ".$rand3." ".$rand4."<br>";
+        if ($rand1 == 50) {
+            echo "oblicz: ".$rand2." * ".$rand3." + ".$rand4."<br>";
+            $rd_sum = $rand2*$rand3+$rand4;
+        }
+        else if ($rand1 == 51) {
+            echo "oblicz: ".$rand2." * ".$rand3." - ".$rand4."<br>";
+            $rd_sum = $rand2*$rand3-$rand4;
+            
+        }
+        else if ($rand5 == 3)
+        {
+            
+            echo "oblicz: ".$rand3." + ".$rand4."<br>";
+            $rd_sum = $rand3 + $rand4;
+        }
+        else {
+            echo "brak rozwiązań";
+        } 
+
+        ?>
         
+
         <p>Podaj wynik:</p><br> 
         <form >
             <input type="text" name="wynikg"> 
             <br>
-            <button type="submit" name="submit" value="submit">Oblicz</button>
+            <button type="submit" name="submit" value="submit">sprawdz</button>
             
         </form>
      <br>
     <?php
         if (isset($_GET['submit'])){
-            $wynikk = $_GET['wynikg'];
-
-            $wynikk = $wynikk + 1;
-            echo "wynik wynosi: ".$wynikk;
-
-        }
+            $wynik = $_GET['wynikg'];
+            
+            if ($wynik == $rd_sum)
+            {
+                echo "wynik poprawny!<br>";
+                echo "suma = ".$rd_sum;
+            }
+            else {
+                echo "wynik niepoprawny";
+            }
+        } 
+        ?>
+    <form >
+        <button type="submit1" name="submit1" value="submit1">reset</button>      
+    </form>
+    <?php
+    if (isset($_GET['submit1'])){
+        header('Location: index.php');  
     
-
-
-    
-    
+    }
     ?>
-
-
     
 </body>
 </html>
