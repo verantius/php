63086a3bd1deb08d2764010b7afa206fbf490793
Adam <adam.midtt@gmail.com>
2021-09-09 22:23:34 +0200

    error

--- eso/login.php
+++ eso/login.php
@@ -2,13 +2,16 @@
 session_start();
 
 require_once "connect.php";
-mysqli_report(MYSQLI_REPORT_STRICT);
+//mysqli_report(MYSQLI_REPORT_STRICT);
 //try {
 
     $polaczenie = new mysqli($host,$db_user,$db_password,$db_name);
 
-    if (mysqli_connect_error())
-        throw new Exception(mysqli_connect_error());
+    if ($polaczenie->connect_errno!=0)
+    {
+        echo "error:".$polaczene->connect_errno."opis: ".$polaczenie->connect_error;
+    }
+       // throw new Exception(mysqli_connect_error());
     else
     {
         $login = $_POST['login'];
@@ -19,30 +22,27 @@ mysqli_report(MYSQLI_REPORT_STRICT);
 
         $sql = "SELECT * FROM users WHERE user='$login' and pass='$haslo'";
         
-        if(
-            $rezultat = $polaczenie->query($sql)
-            //sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
-            //mysqli_real_escape_string($polaczenie,$login),
-            //mysqli_real_escape_string($polaczenie,$haslo)))
-            )
+        if($rezultat = $polaczenie->query($sql))
         {
             $ilu_userow = $rezultat->num_rows;
             if($ilu_userow>0)
             {
-                $_SESSION['zalogowany'] = true;
+                //$_SESSION['zalogowany'] = true;
 
                 $wiersz = $rezultat->fetch_assoc();
-
+                $user=$wiersz['user'];
+/*
                 $_SESSION['id'] = $wiersz['id'];
                 $_SESSION['user'] = $wiersz['user'];
                 $_SESSION['email'] = $wiersz['email'];
                 $_SESSION['gold'] = $wiersz['gold'];
                 $_SESSION['telvar'] = $wiersz['telvar'];
                 $_SESSION['crowns'] = $wiersz['crowns'];
-
                 unset($_SESSION['blad']);
+*/
                 $rezultat->free_result();
-                header('Location: gra.php');
+                echo $user;
+                //header('Location: gra.php');
             }
             else
             {
@@ -64,4 +64,7 @@ catch (Exception $e)
 }
 */
 
+//sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
+//mysqli_real_escape_string($polaczenie,$login),
+//mysqli_real_escape_string($polaczenie,$haslo)))
 ?>
