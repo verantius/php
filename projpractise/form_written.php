78a04619ddf4aa6d844fade094f7bc211e1dcbce
Adam <adam.midtt@gmail.com>
2021-09-23 18:10:53 +0200

    another form

--- projpractise/form_written.php
+++ projpractise/form_written.php
@@ -2,9 +2,27 @@
 if (isset($_POST['submit']))
 {   
     if(empty($_POST['imie']))
-    {
         $error_name = "Jak masz na imię?";
-    }
+    
+    if(empty($_POST['nazwisko']))
+        $error_surname = "Jakie masz nazwisko?";
+    
+    if(empty($_POST['email']))
+        $error_surname = "Jaki posiadasz adress email?";
+    
+    if(empty($_POST['username']))
+        $error_surname = "Podaj jaki chcesz mieć nick";
+
+    if(empty($_POST['birth']))
+        $error_surname = "Kiedy się urodziłeś?";
+
+    if(empty($_POST['haslo1']))
+        $error_surname = "Jakie masz nazwisko?";
+
+
+
+
+
 
 }
 ?>
@@ -16,11 +34,11 @@ if (isset($_POST['submit']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>formularz rejestracyjny</title>
-    <link href="../../twitter-bootstrap/twitter-bootstrap-v2/docs/assets/css/bootstrap.css" rel="stylesheet">
+    <!-- <link href="../../twitter-bootstrap/twitter-bootstrap-v2/docs/assets/css/bootstrap.css" rel="stylesheet"> -->
     <link href="parsely.css" rel="stylesheet">
     <style type="text/css">
     h1 {margin-bottom:20px}
-    input, label {margin-top:2px; margin: bottom 1px; color:#000066; font-size: 16px; padding-right: 7px}
+    /* input, label {margin-top:2px; margin: bottom 1px; color:#000066; font-size: 16px; padding-right: 7px} */
     input[type='checkbox'] {margin-left: 5px}
     .wiadomosc {color: #ff0000}
     .success_msg{color:#006600}
@@ -31,17 +49,13 @@ if (isset($_POST['submit']))
         <h1>Zarejestruj się.</h1>
         <h2>uzupełnij wszystkie dane</h2>
         <form id="rejestration_form" method="post" action="">
-            <label>imię:</label><br>
-            <input type="text" name="imie" autofocus="autofocus" placeholder="podaj Imię">
+            <label>imię:</label>  <br>
+            <input type="text" name="imie" autofocus="autofocus" placeholder="podaj Imię"><br><br>
+            <?php  if(isset($error_name)) echo "<p class='wiadomosc'>".$error_name."</p>";?>
             
-            <?php 
-            if(isset($error_name))
-            echo "<p class='wiadomosc'>".$error_name."</p>";
-            
-            ?><br>
-
             <label>nazwisko:</label><br>
             <input type="text" name="nazwisko" placeholder="podaj Nazwisko"><br><br>
+            <?php  if(isset($error_surname)) echo "<p class='wiadomosc'>".$error_surname."</p>";?>
             
             <label>email:</label><br>
             <input type="text" name="email" placeholder="podaj email"><br><br>
@@ -50,7 +64,7 @@ if (isset($_POST['submit']))
             <input type="text" name="username" placeholder="podaj username"><br><br>
             
             <label>data urodzenia:</label><br>
-            <input type="text" name="birth" placeholder="podaj username"><br><br>
+            <input type="text" name="birth" placeholder="kiedy się urodziłeś? d/m/r"><br><br>
             
             <label>hasło:</label><br>
             <input type="password" name="haslo1"><br>
